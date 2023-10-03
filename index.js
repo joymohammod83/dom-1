ee28d0492b3b752b4ff12bbdd1f30e316c726f77
joymohammod83 <mdjoysarkar83@gmail.com>
2023-10-03 22:35:00 +0600

    idClasstag

---
+++ index.js
@@ -0,0 +1,37 @@
+// getelimentbyid
+
+let id1 = document.getElementById('id1');
+id1.innerText = "inner peace"
+
+let id2 =document.getElementById('id2')
+id2.innerText="document object model"
+
+// getelementbyclassName
+
+let classx = document.getElementsByClassName('classx')[1];
+
+classx.innerText="hey this is joy bae"
+
+let class2 = document.getElementsByClassName('class2')[1];
+class2.innerText = "fruits"
+
+ //GetelementbyName
+
+let head5 = document.getElementsByName('head5')[0];
+head5.innerHTML="HEADING TAG"
+
+let head = document.getElementsByName('head')[0];
+head.innerText="this is header tag"
+
+// getelementBYTagname
+
+let hh = document.getElementsByTagName('h1')[0];
+hh.innerHTML="hellow world this is joy "
+
+
+let pra = document.getElementsByTagName('p')[0];
+pra.innerText="chnage pragraph "
+
+// quarryselector
+
+
